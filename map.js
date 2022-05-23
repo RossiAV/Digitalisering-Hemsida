0051b886951f1d48f6f41857374c3e579b2eeb8e
AdamTegelberg <adam.tegelberg@gmail.com>
2022-05-24 00:54:25 +0200

    Klar på riktigt nu

--- map.js
+++ map.js
@@ -5,4 +5,13 @@ var outout = sessionStorage.getItem('toadress');
 document.getElementById("minadress").innerHTML += outout;
 
 document.getElementById("menycopy").innerHTML += sessionStorage.getItem("page1content");
+
+
+function OrderDone(){
+    document.getElementById("omw").style.visibility = "hidden";
+    document.getElementById("done").style.visibility = "visible";
+}
+
+
+setTimeout( function() { OrderDone(); }, 60000);
   
